77573e028ac6c80995124d6d2fea4df2b423721e
Christian G <chrisgobin94@gmail.com>
2019-10-12 18:34:35 -0400

    axios request

--- imageSearch/App.js
+++ imageSearch/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import SearchBar from "./searchBar";
+const axios = require('axios').default;
 
 class App extends React.Component {
   //callback function to be passed as prop to searchBar child component.
   onValueSubmit(value){
-    console.log(value)
+    //make api request using axios. 
+    axios.get('https://api.unsplash.com/search/photos',{
+      params:{query: value},
+      header:{Authorization: 'Client-ID 2a80ec61a4997acededfaadf11414dbd642432711c839758518df8f2ea6bb737'}
+    })
   }
   
   render() {
