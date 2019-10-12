faf65d6158d9febedd5df152cdea28ec999f0e09
Christian G <chrisgobin94@gmail.com>
2019-10-12 18:04:06 -0400

    added callback function + axios
    passed callback function to child component to test. added axios to app for AJAX requests to API

--- imageSearch/App.js
+++ imageSearch/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 import SearchBar from "./searchBar";
 
 class App extends React.Component {
+  //callback function to be passed as prop to searchBar child component.
+  onValueSubmit(value){
+    console.log(value)
+  }
+  
   render() {
     return (
       <div className="App">
-        <SearchBar></SearchBar>
+        <SearchBar apiCall={this.onValueSubmit}></SearchBar>
       </div>
     );
   }
