b4c93f2d28d432a746f9eb301dedc6b9bbbd96b7
Christian G <chrisgobin94@gmail.com>
2019-10-14 19:55:35 -0400

    unsplash.js file upload

---
+++ imageSearch/api/unsplash.js
@@ -0,0 +1,11 @@
+const axios = require("axios").default;
+const instance = axios.create({
+  baseURL: '"https://api.unsplash.com/search/photos"',
+  timeout: 1000,
+  headers: {
+    Authorization:
+      "Client-ID 2a80ec61a4997acededfaadf11414dbd642432711c839758518df8f2ea6bb737"
+  }
+});
+
+export default instance;
