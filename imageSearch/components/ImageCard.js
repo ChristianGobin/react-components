e606cfd21607e864fccb9c2c441b4ebcfb6967cf
Christian G <chrisgobin94@gmail.com>
2019-10-30 00:58:57 -0400

    added ref to optimize app

--- imageSearch/components/ImageCard.js
+++ imageSearch/components/ImageCard.js
@@ -3,11 +3,29 @@ import React from 'react';
 //the purpose of this component is to determine the height of the image being rendered and allocate the correct amount of space 
 //for the image to not overlap the next image, using the Ref system in react.
 class ImageCard extends React.Component{
-    const {description, urls} = this.props.image;
+    constructor(props){
+        this.state = {spans: 0};
+        super(props);
+        const imageRef = React.createRef();
+    }
+    componentDidMount(){
+        //on load event, run the setSpans callback function
+        this.imageRef.current.addEventListener('load',this.setSpans);
+    }
+    
+    //this function takes the height of the image and determines the amount of space to allocate the image.
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height/10);
+        this.setState({spans : spans});
+    }
+    
+    
     render(){
+        const {description, urls} = this.props.image;
         return(
-            <div>
-              <img src={urls.regular} alt={description}/>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+              <img src={urls.regular} alt={description} ref={this.imageRef}/>
             </div>
         )
     }
