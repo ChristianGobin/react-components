ea5aba29af68c55e0665adc4013153be8279ffaf
Christian G <chrisgobin94@gmail.com>
2019-11-13 21:01:57 -0500

    Removed unnecessary import

--- vidSearch/api/YoutubeApi.js
+++ vidSearch/api/YoutubeApi.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import axios from 'axios';
-
 const KEY = 'AIzaSyARVTgVJSjZ9jZ9uy2vOEpuC5rOFrW-HZE'
 const YoutubeApi = axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/search',
