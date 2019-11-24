849fa42edac6981a6330b7322f1920e7ce0f5a56
Christian G <chrisgobin94@gmail.com>
2019-11-24 13:15:31 -0500

    Updated Component

--- vidSearch/components/VideoList.js
+++ vidSearch/components/VideoList.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import VideoCard from './VideoCard';
-
-VideoList = () => {
+import VideoItem from './VideoItem';
+//deconstruct props component to utilize the videos prop
+const VideoList = ({ videos }) => {
+    //pass video objects into videoItem using props + map
+    //rendered list is a collection of Video Items being rendered
+    const renderedList = videos.map(video => {
+        return (
+            <div className="ui relaxed divided list">
+                <VideoItem vid={video} />
+            </div>
+        )
+    })
     return (
-        <VideoCard />
+        <div>
+            {renderedList}
+        </div>
     )
 }
 
