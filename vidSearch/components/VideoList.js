4743c27dc9ab2b67e91e8f2563321dfc5dd38ede
Christian G <chrisgobin94@gmail.com>
2019-12-02 12:34:22 -0500

    passed function to child component

--- vidSearch/components/VideoList.js
+++ vidSearch/components/VideoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 //deconstruct props component to utilize the videos prop
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos , selectedVid}) => {
     //pass video objects into videoItem using props + map
     //rendered list is a collection of Video Items being rendered
     const renderedList = videos.map(video => {
         return (
             <div className="ui relaxed divided list">
-                <VideoItem vid={video} />
+                <VideoItem vid={video} selectedVid = {selectedVid}/>
             </div>
         )
     })
