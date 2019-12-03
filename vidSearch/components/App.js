d697b0455ceeaa3ae5070b94cd02e6655a4fdc39
Christian G <chrisgobin94@gmail.com>
2019-12-03 14:14:48 -0500

    finishing touches
    update selected vid on form submit to show list of other videos related to search term. 
    on component mount have videos loaded

--- vidSearch/components/App.js
+++ vidSearch/components/App.js
@@ -12,23 +12,34 @@ class App extends React.Component {
     const response = await YoutubeApi.get('/search', {
       params:{ q: value }
     })
-    this.setState({videos: response.data.items})
+    this.setState({videos: response.data.items, selectedVid: response.data.item[0]})
   }
   //Function tracks what vid is selected. 
   selectedVid = (vid) {
     console.log(vid.snippet.title + ' is the selected video.');
     this.setState({selectedVid: vid});
   }
+  componentDidMount(){
+    this.onFormSubmit('youtube api')
+  }
   render() {
     return (
       <div className="vidApp ui container">
-      <h2 style={{textAlign: 'center', color:'#EE82EE'}}>
-        React Video Search by Christian Gobin
-      </h2>
+        <h2 style={{textAlign: 'center', color:'#EE82EE'}}>
+          React Video Search by Christian Gobin
+        </h2>
         <SearchBar onFormSubmit={this.onFormSubmit} />
-        <VideoDetail video={this.state.selectedVid} />
-        <VideoList videos={this.state.videos} selectedVid = {this.selectedVid}/>
-      </div >
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column"
+              <VideoDetail video={this.state.selectedVid} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} selectedVid = {this.selectedVid}/>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
